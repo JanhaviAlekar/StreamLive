42940842595a3fe79ae6bd001bbd7844758dcc41
JanhaviAlekar <janhavialekar@gmail.com>
2024-08-10 18:31:00 +0530

    checking video
    Signed-off-by: JanhaviAlekar <janhavialekar@gmail.com>

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -1,101 +1,100 @@
 "use client";
 
-import { ElementRef, useRef, useState ,useTransition} from "react";
-import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { useState, useTransition, useRef, ElementRef } from "react";
 import { AlertTriangle } from "lucide-react";
+import { IngressInput } from "livekit-server-sdk";
 
 import { createIngress } from "@/actions/ingress";
-
+import { Button } from "@/components/ui/button";
 import {
- Dialog,
- DialogClose,
- DialogContent,
- DialogTitle,
- DialogHeader,
- DialogTrigger
-} from "@/components/ui/dialog"
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
-   Alert,
-   AlertDescription,
-   AlertTitle
-} from "@/components/ui/alert"
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert";
 import {
-   Select,
-   SelectContent,
-   SelectItem,
-   SelectTrigger,
-   SelectValue
-} from "@/components/ui/select"
-import { IngressInput } from "livekit-server-sdk";
-import { toast } from "sonner";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const RTMP = String(IngressInput.RTMP_INPUT);
 const WHIP = String(IngressInput.WHIP_INPUT);
 
-type IngressType = typeof RTMP  | typeof WHIP;
+type IngressType = typeof RTMP | typeof WHIP;
 
-export const ConnectModal = () =>{
-    const closeRef = useRef<ElementRef<"button">>(null);
-    const [isPending ,startTransition] = useTransition();
-    const [ingressType , setIngressType] = useState<IngressType>(RTMP);
+export const ConnectModal = () => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+  const [ingressType, setIngressType] = useState<IngressType>(RTMP);
 
-    const onSubmit = () => {
-        startTransition(()=>{
-            createIngress(parseInt(ingressType))
-              .then(()=>{ 
-                toast.success("Ingress Created");
-                closeRef?.current?.click();
-            })
-              .catch(() => toast.error("Something went wrong"))
+  const onSubmit = () => {
+    startTransition(() => {
+      createIngress(parseInt(ingressType))
+        .then(() => {
+          toast.success("Ingress created");
+          closeRef?.current?.click();
         })
-    }
-    return (
+        .catch(() => toast.error("Something went wrong"));
+    });
+  }
+
+  return (
     <Dialog>
-        <DialogTrigger asChild>
-           <Button variant="primary">
-              Generate Connection
-           </Button>
-        </DialogTrigger>
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>
-                    Generate Connection
-                </DialogTitle>
-            </DialogHeader>
-            <Select
-              disabled= {isPending}
-              value={ingressType}
-              onValueChange={(value) => setIngressType(value)} 
-            >
-                <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Ingress type"/>
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value={RTMP}>RTMP</SelectItem>
-                    <SelectItem value={WHIP}>WHIP</SelectItem>
-                </SelectContent>
-            </Select>
-            <Alert>
-                <AlertTriangle className="w-4 h-4"/>
-                <AlertTitle >Warning!</AlertTitle>
-                <AlertDescription>
-                    This action will reset all active streams using the current Connection
-                </AlertDescription>
-            </Alert>
-            <div className="flex justify-between">
-                <DialogClose ref={closeRef} asChild>
-                    <Button variant="ghost">
-                        Cancel
-                    </Button>
-                </DialogClose>
-                <Button
-                  disabled= {isPending}
-                  onClick={onSubmit}
-                  variant="primary">
-                    Generate
-                  </Button>
-            </div>
-        </DialogContent>
+      <DialogTrigger asChild>
+        <Button variant="primary">
+          Generate connection
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate connection</DialogTitle>
+        </DialogHeader>
+        <Select
+          disabled={isPending}
+          value={ingressType}
+          onValueChange={(value) => setIngressType(value)}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Ingress Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={RTMP}>RTMP</SelectItem>
+            <SelectItem value={WHIP}>WHIP</SelectItem>
+          </SelectContent>
+        </Select>
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning!</AlertTitle>
+          <AlertDescription>
+            This action will reset all active streams using the current connection
+          </AlertDescription>
+        </Alert>
+        <div className="flex justify-between">
+          <DialogClose ref={closeRef} asChild>
+            <Button variant="ghost">
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button
+            disabled={isPending}
+            onClick={onSubmit}
+            variant="primary"
+          >
+            Generate
+          </Button>
+        </div>
+      </DialogContent>
     </Dialog>
-);
-}
+  );
+};
--- components/stream-player/fullscreen-control.tsx
+++ components/stream-player/fullscreen-control.tsx
@@ -1,27 +1,32 @@
-import { Minimize , Maximize } from "lucide-react";
+"use client";
+
+import { Maximize, Minimize } from "lucide-react";
 
 import { Hint } from "@/components/hints";
 
 interface FullscreenControlProps {
-     isFullScreen : boolean,
-     onToggle : () => void;
+  isFullscreen: boolean;
+  onToggle: () => void;
 };
 
-export const FullscreenControl = ({isFullScreen ,onToggle} : FullscreenControlProps) =>{
-    const Icon = isFullScreen ? Minimize : Maximize;
-    
-    const label = isFullScreen ? "Exit full screen" : "Enter full screen";
+export const FullscreenControl = ({
+  isFullscreen,
+  onToggle,
+}: FullscreenControlProps) => {
+  const Icon = isFullscreen ? Minimize : Maximize;
 
-    return (
-        <div className="flex items-center justify-center gap-4">
-            <Hint label={label} asChild>
-                <button
-                  onClick={onToggle}
-                  className="text-white p-1.5 hover:bg-white/10 rounded-lg"
-                >
-                    <Icon className="h-5 w-5"/>
-                </button>
-            </Hint>
-        </div>
-    );
-}
+  const label = isFullscreen ? "Exit fullscreen" : "Enter fullscreen"
+
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <Hint label={label} asChild>
+        <button
+          onClick={onToggle}
+          className="text-white p-1.5 hover:bg-white/10 rounded-lg"
+        >
+          <Icon className="h-5 w-5" />
+        </button>
+      </Hint>
+    </div>
+  );
+};
